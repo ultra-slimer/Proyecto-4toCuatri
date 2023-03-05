37f88bfb51cc279d04314a6c5b1d09f5f370ca70
MrPopas <Mr.Popas1999@gmail.com>
2023-03-06 00:10:23 +0100

    jumping enemy finish?

--- Assets/scripts/JumpingEnemy.cs
+++ Assets/scripts/JumpingEnemy.cs
@@ -20,52 +20,76 @@ public class JumpingEnemy : EnemyFather, ISpawnable<JumpingEnemy>
 
             if (Physics.Raycast(ray, out hit, 0.5f, _layerMask) && hit.transform.GetComponent<EnemyFather>() == null)
             {
-                StartCoroutine("UseSpecialAbility");
+                var a = hit.transform.GetComponent<Cards>();
+                if (!Grid.GetNeighbor(a.tile, Vector2Int.left).occupied)
+                {
+                    StartCoroutine("UseSpecialAbility");
+                }
+                else 
+                {
+                    Attack();
+                }
+              
+            
+                //StartCoroutine("UseSpecialAbility");
+                
             }
             //StartCoroutine("UseSpecialAbility");
         }
         else
         {
-            var ray = new Ray(this.transform.position, this.transform.forward);
-
-            RaycastHit hit;
-            Debug.DrawRay(this.transform.position, this.transform.forward, Color.yellow);
-            if (Physics.Raycast(ray, out hit, 1f, _layerMask))
-            {
-
-                _time += Time.deltaTime;
-                _anim.SetBool("_isFollowing", false);
-                _canWalk = false;
-                if (_attackSpeed <= _time && hit.transform.GetComponent<IDamageable<float>>() != null && hit.transform.GetComponent<EnemyFather>() == null)
-                {
-                    _time = 0;
-
-                    _anim.SetBool("_isAttacking", true);
-                    //Cards pipo = Cards.Instance();
-                    //GetComponent<Cards>().Damage(_damage);
-                    //C.Damage(_damage);
-
-                    if (hit.collider.GetComponent<IEndZone>() != null)
-                    {
-                        //print("a");
-                        hit.collider.GetComponent<IEndZone>().Damage(1, this);
-
-                    }
-                    else
-                    {
-
-                        hit.collider.GetComponent<IDamageable<float>>().Damage(_damage);
-                        AudioManager.Instance().Play("EnemyAttack");
-                    }
-                    hit.collider.GetComponent<IAttackBack>()?.AttackAgressor(_damage, this);
-                }
-            }
-            else _canWalk = true;
+            Attack();
         }
-       
 
-       
+        
 
+    }
+
+    public void Attack()
+    {
+        _allowedJump= false;
+
+        var ray = new Ray(this.transform.position, this.transform.forward);
+
+        RaycastHit hit;
+        Debug.DrawRay(this.transform.position, this.transform.forward, Color.yellow);
+        if (Physics.Raycast(ray, out hit, 1f, _layerMask))
+        {
+
+            _time += Time.deltaTime;
+            _anim.SetBool("_isFollowing", false);
+            _canWalk = false;
+            if (_attackSpeed <= _time && hit.transform.GetComponent<IDamageable<float>>() != null && hit.transform.GetComponent<EnemyFather>() == null)
+            {
+                _time = 0;
+
+                _anim.SetBool("_isAttacking", true);
+                //Cards pipo = Cards.Instance();
+                //GetComponent<Cards>().Damage(_damage);
+                //C.Damage(_damage);
+
+                if (hit.collider.GetComponent<IEndZone>() != null)
+                {
+                    //print("a");
+                    hit.collider.GetComponent<IEndZone>().Damage(1, this);
+
+                }
+                else
+                {
+
+                    hit.collider.GetComponent<IDamageable<float>>().Damage(_damage);
+                    AudioManager.Instance().Play("EnemyAttack");
+                }
+                hit.collider.GetComponent<IAttackBack>()?.AttackAgressor(_damage, this);
+            }
+        }
+        else
+        {
+            _canWalk = true;
+            _allowedJump = true;
+        }
+        
+       
     }
 
     private IEnumerator UseSpecialAbility()
